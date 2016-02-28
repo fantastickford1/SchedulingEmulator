2782dd5aca16ec5203b1f9a551d263de28e6b4bc
Diana Toledo <bexan1818@hotmail.com>
2016-02-28 02:51:23 -0600

    SchedulingSimulator v2.4
    *FCFS v0.1

--- src/Scheduling/FirstInFirstServe.java
+++ src/Scheduling/FirstInFirstServe.java
@@ -1,7 +1,48 @@
 package Scheduling;
 
+import Kernel.Core;
+import Kernel.Process;
+
 /**
  * Created by Karlos on 2/27/2016.
  */
-public class FirstInFirstServe {
+
+import static Kernel.ProcessQueue.processqueue;
+import static Kernel.Core.busy;
+
+public class FirstInFirstServe implements Runnable {
+
+    Core core= new Core();
+    public static Thread t0;
+
+    public void run(){
+        while(true){
+            try {
+                fcFs();
+                Thread.sleep(1000);
+            }catch (Exception e){
+
+            }
+        }
+    }
+
+    public void zombieFication(){
+        t0.interrupt();
+    }
+
+    public void activar(){
+        t0= new Thread(new FirstInFirstServe());
+        t0.start();
+    }
+
+    public void fcFs(){
+
+        Process aux= processqueue.get(0);
+
+        if (!busy){
+           core.serve(aux);
+            processqueue.remove(aux);
+
+        }
+    }
 }
